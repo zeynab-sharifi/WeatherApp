6835a1bede6e5509578fdb17d37b63c62c03d8a8
zeynab <zeynab.sharifi.1378@gmail.com>
2021-12-22 23:37:55 -0800

    add temperature conversion

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-
+//lacation
 const getlocation = async () =>{
     const url = 'http://ip-api.com/json/{query}?fields=country,city,lat,lon,timezone';
     const response = await fetch(url);
@@ -7,6 +7,7 @@ const getlocation = async () =>{
 
     return data
 }
+//weather
  const getweather= async (url) =>{
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a947f993ce697e31c765150735109374`;
     const response = await fetch(url);
@@ -14,7 +15,7 @@ const getlocation = async () =>{
 
     return data;
  }
-
+//Specify night and day
  function getDayNight(){
      let DayNight;
      let d = new Date();
@@ -25,7 +26,7 @@ const getlocation = async () =>{
         DayNight = 'Night';
     }
  }
-
+//icon weather
  function getIcon(weMain){
      let icon;
      switch(weMain){
@@ -51,3 +52,12 @@ const getlocation = async () =>{
         }
         return icon;
  }
+
+ //Temperature conversion
+function getTemp(weTemp){
+    const k = weTemp;
+    const f = (k - 273.15)*9/5+32;
+    const c = k -273.15;
+    return temp = {kel:Math.floor(k), far:Math.floor(f),
+    can:Math.floor(c)};
+}
