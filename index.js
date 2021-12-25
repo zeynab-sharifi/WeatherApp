61f4451b0b375f12ec25e7ca0c78a4e05f4184c2
zeynab <zeynab.sharifi.1378@gmail.com>
2021-12-25 11:45:06 -0800

    add icons file and changes in the file index.js and add style

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
-
-//lacation
-const getlocation = async () =>{
-    const url = 'http://ip-api.com/json/{query}?fields=country,city,lat,lon,timezone';
-    const responded = await fetch(url);
-    const data = await responded.json();
+//location api function
+const getLoc = async () => {
+    const url = 'http://ip-api.com/json/?fields=status,country,city,lat,lon,timezone';
+    const response = await fetch(url);
+    const data = response.json();
 
     return data;
 }
-//weather
- const getweather= async (lat , lon) =>{
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=b23222e676c4424fc8a037fca14ee913`;
-    const responded = await fetch(url);
-    const data = await responded.json();
+//weather api function
+const getWeather = async (lat, lon) => {
+    api = 'b23222e676c4424fc8a037fca14ee913';
+    url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}`;
+    const response = await fetch(url);
+    const data = response.json();
 
     return data;
- }
-//Specify night and day
- function getDayNight(){
-     let DayNight;
-     let d = new Date();
-
-     if (d.getHours() >= 6 && d.getHours() <= 19) {
-           DayNight = 'Day'; 
-    }else{
-        DayNight = 'Night';
-    }
- }
-//icon weather
- function getIcon(weMain){
-     let icon;
-     switch(weMain){
-         case 'Thunderstorm':
+}
+// function icon 
+function getIcon(weMain){
+    let icon;
+    switch (weMain) {
+        case 'Thunderstorm':
             icon = `${weMain}.svg`;
             break;
         case 'Drizzle':
@@ -43,64 +32,86 @@ const getlocation = async () =>{
             icon = `${weMain}.svg`;
             break;
         case 'Clear':
-            const DayNight = getDayNight();
-            icon = `${weMain}-${DayNight}.svg`;
+            const DayOrNigh = getDayOrNight();
+            icon = `${weMain}-${DayOrNigh}.svg`;
+            break;
+        case 'Clouds':
+            icon = `${weMain}.svg`;
             break;
         case 'Atmosphere':
             icon = `${weMain}.png`;
             break;
-        }
-        return icon;
- }
+    }
+    return icon;
+}
+//determination day or night
+function getDayOrNight() {
+    let DayOrNigh;
+    var d = new Date();
 
- //Temperature conversion
+    const hour = d.getHours();
+
+    if (hour >= 6 && hour <= 19) {
+        DayOrNigh = 'Day';
+    } else {
+        DayOrNigh = 'Night';
+    }
+
+    return DayOrNigh;
+}
+//Temperature unit conversion
 function getTemp(weTemp){
     const k = weTemp;
-    const f = (k - 273.15)*9/5+32;
-    const c = k -273.15;
-    return temp = {kel:Math.floor(k), far:Math.floor(f),
-    can:Math.floor(c)};
+    const f = (k - 273.15) * 9/5 + 32;
+    const c = k - 273.15;
+    return temp = {kel:Math.floor(k), far:Math.floor(f), can:Math.floor(c)};
 }
 
-const loacati = document.querySelector('.timezone');
-const icon = document.querySelector('.icon-weather');
-const degSec = document.querySelector('.degree-section');
-const Deg = document.querySelector('.degree-section h2');
+const loti = document.querySelector('.timezone');
+let icon = document.querySelector('.icon');
+const dese = document.querySelector('.degree-section');
+const deg = document.querySelector('.degree-section h3');
 const unit = document.querySelector('.degree-section span');
-const tempDe = document.querySelector('.temperature-description');
+const tede = document.querySelector('.temperature-description');
 
 
-getlocation()
-    .then(locData =>{
-        const timezone = locData.timezone;
-        console.log(timezone)
-        loacati.textContent = timezone;
-        return getweather(locData.lat, locData.lon);
-    }).then(weData => {
-        const weTemp = weData.main.temp;
-        const weMain = weData.weather[0].main;
-        const weDes = weData.weather[0].description;
-        console.log(weTemp,weMain,weDes);
 
 
-        const iconName = getIcon(weMain);
-        icon.innerHTML = `<img src="icons/${iconName}"></img>`;
+window.addEventListener('load', function(){
+    getLoc()
+        .then(locData => {
+            const timeZone = locData.timezone;
+            loti.textContent = timeZone;
+            getWeather(locData.lat, locData.lon)
+                .then(weData => {
+                    const weTemp = weData.main.temp;
+                    const weMain = weData.weather[0].main;
+                    const weDes = weData.weather[0].description;
 
-        Deg.textContent = Math.floor(weTemp);
-        unit.textContent = 'k';
+                    //Change the icon with temperature
+                    const iconName = getIcon(weMain);
+                    icon.innerHTML = `<img src='icons/${iconName}'></img>`;
 
-        dese.addEventListiner('click' , function(e){
-            if(unit.textContent == 'k'){
-                Deg.textContent = getTemp(weTemp).far;
-                unit.textContent = 'F';
-            }
-            else if(unit.textContent == 'F'){
-                Deg.textContent = getTemp(weTemp).can;
-                unit.textContent = 'C';
-            }
-            else{
-                Deg.textContent = getTemp(weTemp).kal;
-                unit.textContent = 'K';
-            }
+                    //Change the temperature display unit by clicking
+                    deg.textContent = Math.floor(weTemp);
+                    unit.textContent = 'K';
+                    dese.addEventListener('click', function(e){
+                        if(unit.textContent == 'K'){
+                            deg.textContent = getTemp(weTemp).far;
+                            unit.textContent = 'F';
+                        } 
+                        else if(unit.textContent == 'F'){
+                            deg.textContent = getTemp(weTemp).can;
+                            unit.textContent = 'C';
+                        }
+                        else{
+                            deg.textContent = getTemp(weTemp).kel;
+                            unit.textContent = 'K';
+                        }
+                    })
+                    //Text weather type
+                    tede.textContent = weDes;
+                    
+                })
         })
-    })
+})
